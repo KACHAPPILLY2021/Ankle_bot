f5c8f2b4133bae6ca94f644857f4e9e2b9857c87
saurabhp369 <saurabhpalande60@gmail.com>
2022-12-04 15:19:13 -0500

    first commit

---
+++ fixed_stiffness.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.params import *
+from scipy.integrate import odeint
+
+# Constructing M, C and H matrices
+M = (m_foot * l_foot**2)/3
+C = Bh + Br
+H = Br + Bh + C
+
+# list to store values
+theta_list = [theta]
+theta_dot_list = [theta_dot]
+kr_list = []
+kh_list = []
+tau_r_list = []
+theta_ddot_list = []
+
+
+
+def calc_theta_d(t,mov):
+    if t<=2:
+        tht =  0.244346*(20*t**3 - 15*t**4 + 3*t**5)/16 - 0.122173
+    else:
+        tht =  0.122173
+    if mov == 1:
+        return tht
+    else:
+        return -tht
+
+def fixed_AIC(y, t, kr_bar, tau_e, M, C, m_foot, l_foot_com, mov ):
+
+    theta_d = calc_theta_d(t, mov)
+    theta_e = theta_d - y[0]
+    if mov == 1:
+        tau_e = -1*tau_e
+    G = -m_foot * 9.81 *l_foot_com * np.cos(y[0])
+    noise = np.random.randint(0, 1)
+    kh = noise + np.exp(np.log(101)/3*t) - 1
+    tau_r = (kr_bar*(theta_e) - Br* y[1] + tau_e)
+    tau_h = kh*theta_e - Bh * y[1]
+    theta_ddot = (1/M)*(tau_r + tau_h - C*y[1] - G)
+    print('tauh', tau_h)
+    theta_ddot_list.append(theta_ddot)
+
+    return [y[1], theta_ddot]
+
+
+# specify the number of movements
+n_movements = 2
+
+for movement in range(n_movements):
+    t = np.linspace(0, 3, 300) 
+    if movement % 2 == 0:
+        # going up
+        mov = 1
+    else:
+        # going down
+        mov =2
+    sol = odeint(fixed_AIC, [theta_list[-1], theta_dot_list[-1]], t, args=(kr_bar, tau_e, M, C, m_foot, l_foot_com, mov))
+    theta_list += list(sol[:,0])
+    theta_dot_list += list(sol[:,1])
+
+theta_d = []
+for movement in range(n_movements):
+    if movement % 2 == 0:
+        # going up
+        mov = 1
+    else:
+        # going down
+        mov =2
+    theta_d += [calc_theta_d(q,mov) for q in t]
+
+plt.plot(theta_d)
+plt.plot(theta_list)
+plt.show()
---
+++ utils/params.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+Br = 1
+beta= 0.0005
+f = 0.9
+g = 0.1
+theta_adm = 0.0244346
+tau_e = 3
+kr_bar = 125
+m_foot = 1.2122
+m_calf = 3.8874
+l_foot = 0.2569
+l_calf = 0.4157
+Bh = 2.51
+m_act = 3.5
+phi = 0.0872665
+l_calf_com = 0.75 * l_calf
+l_foot_com = 0.5 * l_foot
+
+# initial params
+theta = -0.1309
+theta_dot = 0
+theta_ddot = 0
+alpha = 1
+timestep = 0.1
+Emax = -np.inf
